8513193a72374ece4679f281b9a2bc68d177b9d7
Dheeraj Nair <nair.dheeraj@yahoo.co.in>
2018-03-11 03:32:28 +0530

    Added basic functions.

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
+# Initial setup
 from flask import Flask
+import json
+
 app = Flask(__name__)
- 
+
+# Index page 
 @app.route("/")
 def index():
-    return "Hello World!"
- 
+	return_value = {"message":"Welcome to the Edu-Buddy API!"}
+	json_string = json.dumps(return_value)
+	return json_string
+
+# Help page
+@app.route("/help")
+def help():
+	return_value = {"message":"The available commands for this API will be visible here :)"}
+	json_string = json.dumps(return_value)
+	return json_string
+
+# Error page
+@app.errorhandler(404)
+def page_not_found(e):
+    return "No such avaiable command. Refer to the /help for more information.", 404
+
 if __name__ == "__main__":
-    app.run()
+	app.debug = True
+	app.run()
